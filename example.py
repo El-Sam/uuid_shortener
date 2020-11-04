1928caa61baeb1e1fcdb93377bb9ead11aabeb47
El-Sam <elaabidisamira@gmail.com>
2020-11-04 21:42:58 +0200

    Switch to f strings

--- example.py
+++ example.py
@@ -11,21 +11,19 @@ if __name__ == "__main__":
     short_uuid = shortener.shorten(uuid_4)
     unshortened_uuid = shortener.unshorten(short_uuid)
 
-    print('Uuid to shorten: {}'.format(str(uuid_4)))
-    print('shortened uuid: {}'.format(short_uuid))
-    print('unshortened uuid(original uuid): {}'.
-          format(str(shortener.unshorten(short_uuid)), str(uuid_4)))
+    print(f'Uuid to shorten: {uuid_4}')
+    print(f'shortened uuid: {short_uuid}')
+    print(f'unshortened uuid(original uuid): {shortener.unshorten(short_uuid)}')
 
     print("\n\n")
-    print('******** With prefix')
+    print('******** With prefix: meow')
 
-    shortener = UuidShortener('meow')  # ;)
+    shortener = UuidShortener('meow')
     uuid_4 = uuid4()
 
     short_uuid = shortener.shorten(uuid_4)
     unshortened_uuid = shortener.unshorten(short_uuid)
 
-    print('Uuid to shorten: {}'.format(str(uuid_4)))
-    print('shortened uuid: {}'.format(short_uuid))
-    print('unshortened uuid (original uuid): {}'.
-          format(str(shortener.unshorten(short_uuid)), str(uuid_4)))
+    print(f'Uuid to shorten: {uuid_4}')
+    print(f'shortened uuid: {short_uuid}')
+    print(f'unshortened uuid (original uuid): {shortener.unshorten(short_uuid)}')
