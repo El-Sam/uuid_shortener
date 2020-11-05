64c93211347eb05d7bdd207928aa317e6d2ef11f
Guillaume Pasquet <guillaume.pasquet@steel-eye.com>
2020-11-05 10:45:09 +0000

    Added one-way generator to example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from uuid_shortener import UuidShortener
+from uuid_shortener import UuidShortener, ShortUuidGenerator
 from uuid import uuid4
 
 if __name__ == "__main__":
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     print(f'Uuid to shorten: {uuid_4}')
     print(f'shortened uuid: {short_uuid}')
-    print(f'unshortened uuid(original uuid): {shortener.unshorten(short_uuid)}')
+    print(f'unshortened uuid(original uuid): {unshortened_uuid}')
 
     print("\n\n")
     print('******** With prefix: meow')
@@ -26,4 +26,15 @@ if __name__ == "__main__":
 
     print(f'Uuid to shorten: {uuid_4}')
     print(f'shortened uuid: {short_uuid}')
-    print(f'unshortened uuid (original uuid): {shortener.unshorten(short_uuid)}')
+    print(f'unshortened uuid (original uuid): {unshortened_uuid}')
+
+    print("\n\n")
+    print('******** With a one-way generator')
+
+    easy_generator = ShortUuidGenerator()
+    short_uuid = easy_generator()
+    # Reusing the previously created shortener to unshorten.
+    unshortened_uuid = shortener.unshorten(short_uuid)
+
+    print(f'One-way shortened uuid: {short_uuid}')
+    print(f'unshortened uuid (original uuid): {unshortened_uuid}')
